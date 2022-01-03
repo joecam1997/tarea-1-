7d417b7cabd28e8468a4de3092bb06a12e27da1a
joecam1997 <95659325+joecam1997@users.noreply.github.com>
2022-01-03 15:27:42 -0500

    Add files via upload

---
+++ ejercicio 29.py
@@ -0,0 +1,11 @@
+#En un almacén se hace un 20% de descuento a los clientes cuya compra supere
+# los Bs 1000, se desea que realice un algoritmo el cual tome por entrada el monto a
+# pagar por el cliente y arroje como salida el monto aplicando el descuento de ser
+# necesario.
+def (self):
+     
+if __name__ == '__main__':
+     print("Ingrese Monto : ")
+     monto = float(input())
+     if monto>1000:
+          print("Tendrá un Descuento del 20% : ",monto*0.20) 
