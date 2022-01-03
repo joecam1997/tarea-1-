5880a37f847053c7b7c7f719ebdcb0b8c4c88cc0
joecam1997 <95659325+joecam1997@users.noreply.github.com>
2022-01-03 15:39:00 -0500

    Add files via upload

---
+++ ejercicio 30.py
@@ -0,0 +1,9 @@
+class deber:
+    #Dado un número entero N, calcular e informar al usuario cuántos dígitos tiene dicho número.
+  def ejercicio1(self):
+    num = int(input("Ingresa un numero: "))
+    contador = len(str(num))
+    print("El numero tiene %s digitos" % (contador)) 
+    
+    
+    
